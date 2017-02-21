6274139160396401bbd6a88e746a2b914ce92b41
bkbeasley <bbeasley344@gmail.com>
2017-02-21 15:42:59 -0600

    Remove html grid and add dynamic grid via jQuery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
-	$column = $(".column");
+	//Store the grid
+	var grid = $(".grid");
+	
+	//Set the number of rows and columns the grid should have
+	rowSize = 16;
+	columnSize = 16;
+	
+	//Create rows and columns of appropriate number
+	for(i = 0; i < rowSize; i++) {
+		grid.append("<tr>");
+		
+		for(j = 0; j < columnSize; j++) {
+			grid.append("<td class='cell'></td>");
+		}
 
-	$column.click(function() {
-		$(this).css("background-color","#ffffff");
+		grid.append("</tr>");
+	}
+
+	$(".cell").hover(function() {
+		$(this).css("background-color", "white");
+	}, function() {
+
 	});
+	
 });
