eaf85dd70d4dbd9138427d3c40cd1bea8a0abf60
bkbeasley <bbeasley344@gmail.com>
2017-02-22 12:42:29 -0600

    Replace table with div. Change page design using bootstrap.

--- js/script.js
+++ js/script.js
@@ -6,59 +6,52 @@ $(document).ready(function() {
 });
 
 
-	//Initialize and display a grid of appropriate size
-	function displayGrid(gridSize = null) {
-		//Store the grid
-		var grid = $(".grid");
+//Initialize and display a grid of appropriate size
+function displayGrid(gridArea = null) {
+	//Store the grid element
+	var grid = $(".grid");
 
-		var defaultRowSize;
-		var defaultColumnSize;
+	var defaultRowSize;
+	var defaultColumnSize;
 
-		//Set the default number of rows and columns the grid should have
-		if(gridSize === null) {
-			defaultRowSize = 16;
-			defaultColumnSize = 16;
-		}
-		else {
-			defaultRowSize = gridSize;
-			defaultColumnSize = gridSize;
-		}
-
-		//Create rows and columns of appropriate number
-		for(i = 0; i < defaultRowSize; i++) {
-			grid.append("<tr class='row'>");
-			
-			for(j = 0; j < defaultColumnSize; j++) {
-				grid.append("<td class='cell'></td>");
-			}
-
-			grid.append("</tr>");
-		}
+	//Set the default number of rows and columns the grid should have
+	if(gridArea === null) {
+		gridArea = 16;
 	}
 	
-	function paintCell() {
-		//Change the color of each cell a user hovers over
-		$(document).on("mouseover", ".cell", function() {
-			$(this).addClass("paint");
-		});
-	}
 
-	function clearGrid() {
-		//Clear the grid
-		$("#clear-grid").click(function() {
-			$(".cell").removeClass("paint");
-		});
+	//Create rows and columns of appropriate number
+	for(i = 0; i < gridArea * gridArea; i++) {
+		grid.append("<div class='cell'></div>");
 	}
+	$(".cell").css("height", 500 / gridArea);
+		$(".cell").css("width", 500 / gridArea);
 	
-	function setGridSize() {
-		//Change the size of the grid according to user input
-		$("#grid-size").click(function() {
-			var userSize = prompt("Enter a new grid size:", "");
+}
 
-			size = userSize;
-			$(".cell").detach();
-			displayGrid(userSize);
-		});
-	}
+function paintCell() {
+	//Change the color of each cell a user hovers over
+	$(document).on("mouseover", ".cell", function() {
+		$(this).addClass("paint");
+	});
+}
+
+function clearGrid() {
+	//Clear the grid
+	$("#clear-grid").click(function() {
+		$(".cell").removeClass("paint");
+	});
+}
+
+function setGridSize() {
+	//Change the size of the grid according to user input
+	$("#grid-size").click(function() {
+		var userSize = prompt("Enter a new grid size:", "");
+
+		size = userSize;
+		$(".cell").detach();
+		displayGrid(userSize);
+	});
+}
 	
 
