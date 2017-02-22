0bb3ac69048121a3173c2b548e78095ec72dbecd
bkbeasley <bbeasley344@gmail.com>
2017-02-21 21:37:31 -0600

    Add clear table and resize table features. Center table.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,64 @@
 $(document).ready(function() {
-	//Store the grid
-	var grid = $(".grid");
-	
-	//Set the number of rows and columns the grid should have
-	rowSize = 16;
-	columnSize = 16;
-	
-	//Create rows and columns of appropriate number
-	for(i = 0; i < rowSize; i++) {
-		grid.append("<tr>");
-		
-		for(j = 0; j < columnSize; j++) {
-			grid.append("<td class='cell'></td>");
+	displayGrid();
+	clearGrid();
+	paintCell();
+	setGridSize();
+});
+
+
+	//Initialize and display a grid of appropriate size
+	function displayGrid(gridSize = null) {
+		//Store the grid
+		var grid = $(".grid");
+
+		var defaultRowSize;
+		var defaultColumnSize;
+
+		//Set the default number of rows and columns the grid should have
+		if(gridSize === null) {
+			defaultRowSize = 16;
+			defaultColumnSize = 16;
+		}
+		else {
+			defaultRowSize = gridSize;
+			defaultColumnSize = gridSize;
 		}
 
-		grid.append("</tr>");
+		//Create rows and columns of appropriate number
+		for(i = 0; i < defaultRowSize; i++) {
+			grid.append("<tr class='row'>");
+			
+			for(j = 0; j < defaultColumnSize; j++) {
+				grid.append("<td class='cell'></td>");
+			}
+
+			grid.append("</tr>");
+		}
+	}
+	
+	function paintCell() {
+		//Change the color of each cell a user hovers over
+		$(document).on("mouseover", ".cell", function() {
+			$(this).addClass("paint");
+		});
 	}
 
-	$(".cell").hover(function() {
-		$(this).css("background-color", "white");
-	}, function() {
-
-	});
+	function clearGrid() {
+		//Clear the grid
+		$("#clear-grid").click(function() {
+			$(".cell").removeClass("paint");
+		});
+	}
 	
-});
+	function setGridSize() {
+		//Change the size of the grid according to user input
+		$("#grid-size").click(function() {
+			var userSize = prompt("Enter a new grid size:", "");
+
+			size = userSize;
+			$(".cell").detach();
+			displayGrid(userSize);
+		});
+	}
+	
+
